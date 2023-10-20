56cb93b162d7aee423d6e1156db29472c74779cc
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-10-20 10:08:11 -0400

    worked on game api

--- controllers/game_routes.js
+++ controllers/game_routes.js
@@ -2,17 +2,30 @@ const router = require('express').Router();
 const Game = require('../models/Game');
 const axios = require('axios');
 
+const baseURL = 'https://api.rawg.io/api'
+
 let gameList = [];
 
 router.get('/game', async (req, res) => {
   try {
-    const games = await axios.get('https://api.rawg.io/api/games?key=c604fbdc699c49b98500aa3c47cfd63b');
+    const games = await axios.get(`${baseURL}/games?key=${process.env.API_KEY}`);
+
+    // const games = await axios.get('https://api.rawg.io/api/gameskey=c604fbdc699c49b98500aa3c47cfd63b');
+
+    console.log(games);
+
+    gameList = games.data.results.map(game => {
+      return {
+        id: game.id,
+        name: game.name
+      };
+    });
     
-    console.log(games.data.results[0]);
-    res.send('Games');
+    res.json(games.data);
 
   } catch (err) {
     console.log(err);
+    res.send(err);
     // Set our session errors array to an array of just sequelize error message stings
     // req.session.errors = error.errors.map(err => err.message);
   }
